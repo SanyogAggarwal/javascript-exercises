1718740f1486ce9dd5180f4ebb02636387b756a9
SANYOG AGGARWAL <sanyogaggarwal7@gmail.com>
2024-09-18 18:43:47 +0530

    done 02_ and 01_

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
+const repeatString = function(string,num) {
+    
+    if(num>=0){
+        var Newstring = "";
+    for(let i = 1 ; i <= num ; ++i ){
+     Newstring += string;
+     
+    }
+}else{
+   return 'ERROR';
+}
+return Newstring;  
 };
-
+console.log(repeatString("hey",3));
+ 
 // Do not edit below this line
 module.exports = repeatString;
