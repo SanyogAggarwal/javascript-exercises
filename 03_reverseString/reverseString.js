7a7f4cdb90972c129154be30a6a4b6da0c4eb444
SANYOG AGGARWAL <sanyogaggarwal7@gmail.com>
2024-09-19 18:16:33 +0530

    done 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(Input) {
+    var MyArray = Input.split("");
+    const RevArray = MyArray.reverse();
+    const finalString = RevArray.join("");
+    return finalString;
 };
+
+console.log(reverseString("hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(Arrray , ...num) {
+    for(const input of num){
+    while(Arrray.indexOf(input) !== -1){
+    var RemoveElement = Arrray.splice(Arrray.indexOf(input) , 1)
+    }
+}
+return Arrray;
 };
-
+console.log(removeFromArray([1,2,2,4,5] ,2 ))
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// test 3 were we want to remove dublicate elements was diffucult
