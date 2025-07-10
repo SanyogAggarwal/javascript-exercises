4d7ecc5a656f910b97400c6142b5653177d6d635
SANYOG AGGARWAL <sanyogaggarwal7@gmail.com>
2025-07-10 19:14:00 +0530

    calculator done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
-};
+const add = function(...nums) {
+ 
+  let sum = nums.reduce((accu , current) => {
+    return accu + current
+  }, 0) 
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+  return sum
 
 };
 
-const power = function() {
-	
+const subtract = function(...nums) {
+  if (nums.length == 0) {
+    return 0
+  }else{
+	let subNum = nums.reduce((accu , current) => {
+    return accu - current
+  })
+  return subNum
+}
 };
 
-const factorial = function() {
-	
+const sum = function(nums) {
+  let total = nums.reduce((accu , current) => {
+    return accu + current
+  },0)
+  return total;
+};
+
+const multiply = function(nums) {
+  
+  let muti = nums.reduce((accu , current ) => {
+  return accu*current
+ },1)
+
+ return muti
+  
+};
+
+const power = function(num , power) {
+	return num**power
+};
+
+const factorial = function(num) {
+  let facto = 1;
+	for (let index = num; index > 0; index--) {
+    facto *= index
+  }
+  return facto
 };
 
 // Do not edit below this line
