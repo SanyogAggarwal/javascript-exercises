44a22b7db69f8709b3f570860214921be57dd6c5
SANYOG AGGARWAL <sanyogaggarwal7@gmail.com>
2025-07-10 20:24:43 +0530

    panlindromes done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let oriString = input.toLowerCase().replaceAll(/[^a-z0-9]/g , "")
+    let reverseString = oriString.split("").reverse().join("")
+  
+    return oriString == reverseString
 };
 
 // Do not edit below this line
