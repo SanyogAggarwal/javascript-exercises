60c465a5c4029701351bc48acba84c91985b8803
SANYOG AGGARWAL <sanyogaggarwal7@gmail.com>
2025-07-11 16:31:22 +0530

    fibonacci done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let num = Number(input)
+    if (num < 0) {
+    return "OOPS"
+}else if(num == 0 ){
+    return 0
+}else if (num == 1 || num == 2){
+    return 1
+
+}else{
+    let num1 = 1
+    let num2 = 1
+    for (let i = 3; i <= num ; i++) {
+        let sum = num1 + num2
+        num1 = num2
+        num2 = sum
+    }
+return num2
+}
+
 
 };
 
