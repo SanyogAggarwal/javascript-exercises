a600fc662aec1064b7a0f415d6b32bd09962f1b9
SANYOG AGGARWAL <sanyogaggarwal7@gmail.com>
2025-07-14 17:44:41 +0530

    findTheOldest done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(input) {
+let result = input.reduce((accu , curent) => {
+  
+  if (!("yearOfDeath" in accu)) {
+         accu.yearOfDeath = new Date().getFullYear()
+}
 
-};
+ if (!("yearOfDeath" in curent)) {
+    curent.yearOfDeath = new Date().getFullYear()
+ }
+
+ let age1 = accu.yearOfDeath - accu.yearOfBirth  
+ let age2 = curent.yearOfDeath - curent.yearOfBirth
+
+ return age1 > age2 ? accu : curent
+})
+
+return result
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
